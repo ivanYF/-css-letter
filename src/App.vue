457e60aebe5d63c703d96e6ade1bcae97259c9b1
ivan <liuyongivan@126.com>
2018-05-31 22:06:50 +0800

    add letter fghij

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,43 @@
         <div class="font" data-id="E">
             <div class="e"></div>
         </div>
-        <div class="font" data-id=""><div class=""></div></div>
-        <div class="font" data-id=""><div class=""></div></div>
-        <div class="font" data-id=""><div class=""></div></div>
-        <div class="font" data-id=""><div class=""></div></div>
-        <div class="font" data-id=""><div class=""></div></div>
-
+        <div class="font" data-id="F">
+            <div class="f"></div>
+        </div>
+        <div class="font" data-id="G">
+            <div class="g"></div>
+        </div>
+        <div class="font" data-id="H">
+            <div class="h"></div>
+        </div>
+        <div class="font" data-id="I">
+            <div class="i"></div>
+        </div>
+        <div class="font" data-id="J">
+            <div class="j"></div>
+        </div>
+        <div class="font" data-id="K">
+            <div class="k"></div>
+        </div>
+        <div class="font" data-id="L">
+            <div class="l"></div>
+        </div>
+        <div class="font" data-id="M">
+            <div class="m"></div>
+        </div>
+        <div class="font" data-id="N">
+            <div class="n"></div>
+        </div>
+        <div class="font" data-id="O">
+            <div class="o"></div>
+        </div>
+        <div class="font" data-id="P">
+            <div class="p"></div>
+        </div>
+        <!-- <div class="font" data-id="">
+            <div class=""></div>
+        </div> -->
+        
     </div>
 </template>
 
@@ -47,6 +78,127 @@ export default {
         margin: 20px;
         overflow: hidden;
         border: 1px solid #999;
+        .j{
+            width: 50px;
+            height: 80px;
+            border-width:0 15px 15px 14px;
+            border-color:#000000;
+            border-style:solid;
+            border-radius:0 0 75%75%;
+            position: relative;
+            left: 50px;
+            top:70px;
+            &:after{
+                content: '';
+                position: absolute;
+                width: 30px;
+                height: 30px;
+                right: -15px;
+                top:-40px;
+                border-width: 15px 15px 0 0;
+                border-color:#000000;
+                border-style:solid;
+            }
+            &:before{
+                content: '';
+                position: absolute;
+                left: -35px;
+                top:-10px;
+                width: 50px;
+                height: 80px;
+                background: #fff;
+                transform:rotate(-30deg);
+            }
+        }
+        .i{
+            width: 60px;
+            height: 120px;
+            border-width: 15px 0 15px 0;
+            border-style: solid;
+            border-color: #000;
+            position: relative;
+            left: 65px;
+            top: 25px;
+            &:after{
+                content: '';
+                width: 15px;
+                height: 120px;
+                background-color: #000;
+                position: absolute;
+                top:0px;
+                left: 22.5px;
+            }
+        }
+        .h{
+            width: 55px;
+            height: 120px;
+            border-width: 0 15px 0 15px;
+            border-style: solid;
+            border-color: #000;
+            position: relative;
+            left: 55px;
+            top: 35px;
+            &:after{
+                content: '';
+                width: 60px;
+                height: 13px;
+                background-color: #000;
+                position: absolute;
+                top:50px;
+                left: 0;
+            }
+        }
+        .g{
+            width: 80px;
+            height: 90px;
+            border: 15px solid #000;
+            border-radius: 50%;
+            position: relative;
+            left: 50px;
+            top: 40px;
+            &:after{
+                content: '';
+                width: 65px;
+                height: 65px;
+                background: #fff;
+                position: absolute;
+                right: -30px;
+                top:12px;
+                transform:rotate(45deg);
+                z-index: 1;
+            }
+            &:before{
+                z-index: 2;
+                position: absolute;
+                right: 0;
+                top:60px;
+                content: '';
+                width: 30px;
+                height: 30px;
+                border-width: 15px 15px 0 0;
+                border-style: solid;
+                border-color: #000;
+            }
+        }
+        .f{
+            width: 60px;
+            height: 110px;
+            border-width: 15px 0 0 17px;
+            border-style: solid;
+            border-color: #000;
+            position: relative;
+            left: 55px;
+            top: 30px;
+            &:after{
+                content: '';
+                width: 60px;
+                height: 15px;
+                background-color: #000;
+                position: absolute;
+                top:40px;
+                left: 0;
+            }
+        }
         .e{
             width: 60px;
             height: 100px;
